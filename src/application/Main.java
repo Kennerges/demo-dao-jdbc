047bfaa1e0fd75d6bc653d1f7fb54e8492e6afd8
Kennerges <kennerges@gmail.com>
2020-06-10 06:01:08 -0300

    Department entity class

---
+++ src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import model.entities.Department;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Department obj = new Department(2, "Books");
+
+        System.out.println(obj);
+
+    }
+}
